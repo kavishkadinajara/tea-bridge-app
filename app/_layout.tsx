5e73c772654f7df77ae33bfe4cf167b36b44bf34
lafesto <kavishkadinajara@gmail.com>
2024-11-16 21:38:24 +0530

    Add initial project setup with configuration files, nativewind integration, and basic components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,8 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import '../global.css';
+import 'expo-dev-client';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
